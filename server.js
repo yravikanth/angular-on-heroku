cf722f06ac575144fabb2e2822542ac85b6fb1a2
yravikanth <yravikanth@gmail.com>
2020-09-16 12:44:01 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/sample-app1'));
+console.log("__dirname ===> " + __dirname);
+app.use(express.static(__dirname + '/dist/SampleApp1'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/myapp/index.html'));});
+'/dist/SampleApp1/index.html'));});
 app.listen(process.env.PORT || 8080);
