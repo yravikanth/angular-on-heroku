c6c8d5cf27366bdcbfef7e8b850acb3c1a604635
yravikanth <yravikanth@gmail.com>
2020-09-17 11:50:24 +0530

    updates to deploy to heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RestService } from './rest.service';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SampleApp1';
+  names: any;
+  constructor(private restService: RestService) { }
+  
+  ngOnInit() {
+    this.restService.getNames().subscribe((resp: any) => {
+      this.names = resp;
+      console.log("Names: " + this.names);
+    });
+  }
 }
---
+++ src/app/rest.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/internal/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class RestService {
+  constructor(private http: HttpClient) { }
+  private endpoint = 'http://localhost:3000/';
+  
+  getNames(): Observable<any> {
+    return this.http.get(this.endpoint + "names").pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+}
